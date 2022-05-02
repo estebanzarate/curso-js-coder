1dda1b7e684366524d5354646049d8109409b48f
estebanzarate <esteban-zarate@hotmail.com>
2022-05-02 20:47:57 -0300

    simulador interactivo

---
+++ clase3/simulador_interactivo/js/script.js
@@ -0,0 +1,45 @@
+function Producto(name, price) {
+    this.name = name;
+    this.price = price;
+    this.quantity = 0;
+}
+
+const producto1 = new Producto("Producto1", 100);
+const producto2 = new Producto("Producto2", 200);
+const producto3 = new Producto("Producto3", 300);
+
+let subTotal = 0;
+
+function start() {
+    let select = parseInt(prompt("Productos disponibles: \n\t ingresa el número del producto que quieres comprar \n\n 1 -Producto1 \n 2 -Producto2 \n 3 -Producto3 \n\n 9- Finalizar compra \n 0- Salir"));
+    quantity(select);
+}
+
+function quantity(select) {
+    if(select === 9) return total(subTotal);
+    if(select === 0) return alert("Volvé pronto!!");
+    let howMuch = parseInt(prompt("Cuántos vas a llevar?"));
+    switch (select) {
+        case 1:
+            subTotal += producto1.price * howMuch;
+            break;
+        case 2:
+            subTotal += producto2.price * howMuch;
+            break;
+        case 3:
+            subTotal += producto3.price * howMuch;
+            break;
+    }
+    start();
+}
+
+function total(subTotal) {
+    if(subTotal === 0) {
+        alert("Tu carrito está vacío. \n Selecciona un producto de la lista");
+        return start();
+    }
+    alert("El total a pagar es: $" + subTotal);
+    alert("Gracias por tu compra");
+}
+
+start();
