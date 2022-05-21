8d5e88c005a3c11f5d6c3ed8c8ae5e2ab9d16b9d
estebanzarate <esteban-zarate@hotmail.com>
2022-05-21 20:44:11 -0300

    queries

--- clase4/js/index.js
+++ clase4/js/index.js
@@ -1,5 +1,5 @@
 class Student {
-    constructor(name, lastName, birth, tel) {
+    constructor(name, lastName, birth, dni, tel) {
         this.id = Student.students.length + 1;
         this.name = name;
         this.lastName = lastName;
@@ -22,13 +22,15 @@ const addStudent = e => {
             <td>${$inputs[1].value}</td>
             <td>${$inputs[2].value}</td>
             <td>${$inputs[3].value}</td>
+            <td>${$inputs[4].value}</td>
         </tr>
     `;
     const student = {
         name: $inputs[0].value,
         lastName: $inputs[1].value,
         birth: $inputs[2].value,
-        tel: $inputs[3].value
+        tel: $inputs[3].value,
+        dni: $inputs[4].value
     }
     createStudent(student);
     $inputs.forEach(el => {
@@ -38,8 +40,8 @@ const addStudent = e => {
 }
 
 const createStudent = values => {
-    const { name, lastName, birth, tel } = values;
-    Student.students.push(new Student(name, lastName, birth, tel));
+    const { name, lastName, birth, dni, tel } = values;
+    Student.students.push(new Student(name, lastName, birth, dni, tel));
 }
 
 $formToAddStudent.addEventListener('submit', addStudent);
