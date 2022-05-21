1d0468e2e9d96be97cc1b3de7d8e44956e0856af
estebanzarate <esteban-zarate@hotmail.com>
2022-05-21 03:20:35 -0300

    style

--- clase4/js/index.js
+++ clase4/js/index.js
@@ -1,8 +1,10 @@
 class Student {
-    constructor(name, lastName) {
+    constructor(name, lastName, birth, tel) {
         this.id = Student.students.length + 1;
         this.name = name;
         this.lastName = lastName;
+        this.birth = birth;
+        this.tel = tel;
     }
     static students = [];
 }
@@ -11,9 +13,7 @@ const d = document;
 const $formToAddStudent = d.querySelector('#form-add-student');
 const $tableStudentsBody = d.querySelector('#table-students-body');
 
-console.log($tableStudentsBody);
-
-function addStudent(e) {
+const addStudent = e => {
     e.preventDefault();
     $inputs = d.querySelectorAll('input[required]');
     $tableStudentsBody.innerHTML += `
@@ -21,12 +21,25 @@ function addStudent(e) {
             <td>${$inputs[0].value}</td>
             <td>${$inputs[1].value}</td>
             <td>${$inputs[2].value}</td>
+            <td>${$inputs[3].value}</td>
         </tr>
     `;
+    const student = {
+        name: $inputs[0].value,
+        lastName: $inputs[1].value,
+        birth: $inputs[2].value,
+        tel: $inputs[3].value
+    }
+    createStudent(student);
     $inputs.forEach(el => {
         el.value = "";
     });
     $inputs[0].focus();
+}
+
+const createStudent = values => {
+    const { name, lastName, birth, tel } = values;
+    Student.students.push(new Student(name, lastName, birth, tel));
 }
 
 $formToAddStudent.addEventListener('submit', addStudent);
